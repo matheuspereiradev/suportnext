56d0924527e993b9bf153b02c7965f0274aaa3aa
matheus <matheuslima20111997@gmail.com>
2021-06-12 17:12:16 -0300

    configurando i18n

---
+++ src/translations/config.ts
@@ -0,0 +1,9 @@
+export const defaultLocale = 'es' as const
+
+export const locales = ['es', 'en', 'pt'] as const
+
+export const languageNames = {
+  es: 'Español',
+  en: 'English',
+  pt: 'Português'
+}
---
+++ src/translations/strings.ts
@@ -0,0 +1,15 @@
+import { Strings } from './types'
+
+const strings: Strings = {
+  es: {
+    logout: 'Salir',
+  },
+  en: {
+    logout: 'Logout',
+  },
+  pt: {
+    logout: 'Sair',
+  }
+}
+
+export default strings
