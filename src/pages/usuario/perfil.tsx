10115b010a30a865df779bf9e70c2b1eb2bcf8f4
matheus <matheuslima20111997@gmail.com>
2021-06-18 16:47:05 -0300

    melhoriasde layout

--- src/pages/usuario/perfil.tsx
+++ src/pages/usuario/perfil.tsx
@@ -23,56 +23,73 @@ export default function Perfil() {
 
     const { user } = useAuth();
     const { addToast } = useToast();
-    
-    const { register, handleSubmit,setValue, formState: { errors } } = useForm<FormData>();
+
+    const { register, handleSubmit, setValue, formState: { errors } } = useForm<FormData>();
     const onSubmit = handleSubmit(async (data) => {
-        try{
-            const res = await browserAPIRequest.put('/user',data);
-            addToast({title:"Sucesso",description:"Perfil atualizado com suceso",type:"success"})
+        try {
+            const res = await browserAPIRequest.put('/user', data);
+            addToast({ title: "Sucesso", description: "Perfil atualizado com suceso", type: "success" })
             Router.push(`/ticket/${res.data.id}`);
-        }catch(e){
-            addToast({title:"Erro",description:"Erro ao atualizar perfil",type:"error"})
+        } catch (e) {
+            addToast({ title: "Erro", description: "Erro ao atualizar perfil", type: "error" })
         }
-        
+
     });
 
-    useEffect(()=>{
-        if(user){
+    useEffect(() => {
+        if (user) {
             setValue("name", user.name)
             setValue("surname", user.surname)
             setValue("gender", user.gender)
         }
-    },[user])
+    }, [user])
 
     return (
         <LayoutCloseButton titleKey="perfil">
             <main className={styles.container}>
                 <div className={styles.header}>
                     <h1>Editar perfil</h1>
+                    <hr />
                     <br />
                 </div>
                 <div className={styles.content}>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <label>Nome:</label>
-                        <Error message={errors.name?.message}/><br />
-                        <input type="text" placeholder="Nome" {...register("name", { required: { value: true, message: "O campo título deve ter pelo menos 3 caracteres" }, minLength: { value: 3, message: "O campo título deve ter pelo menos 3 caracteres" }, maxLength: { value: 100, message: "O campo título deve ter menos de 100 caracteres" } })} />
-                        <label>Sobrenome:</label>
-                        <Error message={errors.name?.message}/><br />
-                        <input type="text" placeholder="Sobrenome" {...register("surname", { required: { value: true, message: "O campo título deve ter pelo menos 3 caracteres" }, minLength: { value: 3, message: "O campo título deve ter pelo menos 3 caracteres" }, maxLength: { value: 100, message: "O campo título deve ter menos de 100 caracteres" } })} />
-                        <label>Sexo:</label>
-                        <select>
-                            <option value="F" selected={user?.gender==="F"}>Feminino</option>
-                            <option value="M" selected={user?.gender==="M"}>Masculino</option>
-                        </select>
-                        <label>Senha:</label>
-                        <Error message={errors.password?.message}/><br />
-                        <input type="password" placeholder="Senha" {...register("password", { required: { value: true, message: "O campo título deve ter pelo menos 3 caracteres" }, minLength: { value: 3, message: "O campo título deve ter pelo menos 3 caracteres" }, maxLength: { value: 100, message: "O campo título deve ter menos de 100 caracteres" } })} />
-                        <label>Repita sua senha:</label>
-                        <Error message={errors.password2?.message}/><br />
-                        <input type="password" placeholder="Repita sua senha" {...register("password2", { required: { value: true, message: "O campo título deve ter pelo menos 3 caracteres" }, minLength: { value: 3, message: "O campo título deve ter pelo menos 3 caracteres" }, maxLength: { value: 100, message: "O campo título deve ter menos de 100 caracteres" } })} />
-                        
-                        
-                        
+                        <div className={styles.row}>
+                            <div className={styles.column}>
+                                <label>Nome:</label>
+                                <Error message={errors.name?.message} /><br />
+                                <input type="text" placeholder="Nome" {...register("name", { required: { value: true, message: "O campo título deve ter pelo menos 3 caracteres" }, minLength: { value: 3, message: "O campo título deve ter pelo menos 3 caracteres" }, maxLength: { value: 100, message: "O campo título deve ter menos de 100 caracteres" } })} />
+                            </div>
+                            <div className={styles.column}>
+                                <label>Sobrenome:</label>
+                                <Error message={errors.name?.message} /><br />
+                                <input type="text" placeholder="Sobrenome" {...register("surname", { required: { value: true, message: "O campo título deve ter pelo menos 3 caracteres" }, minLength: { value: 3, message: "O campo título deve ter pelo menos 3 caracteres" }, maxLength: { value: 100, message: "O campo título deve ter menos de 100 caracteres" } })} />
+                            </div>
+                            <div className={styles.column}>
+                                <label>Email:</label>
+                                <input type="text" placeholder="Email" value={user?.email} disabled={true}/>
+                            </div>
+                            <div className={styles.column}>
+                                <label>Sexo:</label><br />
+                                <select>
+                                    <option value="F" selected={user?.gender === "F"}>Feminino</option>
+                                    <option value="M" selected={user?.gender === "M"}>Masculino</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <div className={styles.column}>
+                                <label>Senha:</label>
+                                <Error message={errors.password?.message} /><br />
+                                <input type="password" placeholder="Senha" {...register("password", { required: { value: true, message: "O campo título deve ter pelo menos 3 caracteres" }, minLength: { value: 3, message: "O campo título deve ter pelo menos 3 caracteres" }, maxLength: { value: 100, message: "O campo título deve ter menos de 100 caracteres" } })} />
+                            </div>
+                            <div className={styles.column}>
+                                <label>Repita sua senha:</label>
+                                <Error message={errors.password2?.message} /><br />
+                                <input type="password" placeholder="Repita sua senha" {...register("password2", { required: { value: true, message: "O campo título deve ter pelo menos 3 caracteres" }, minLength: { value: 3, message: "O campo título deve ter pelo menos 3 caracteres" }, maxLength: { value: 100, message: "O campo título deve ter menos de 100 caracteres" } })} />
+                            </div>
+                        </div>
+
                         <div className={styles.buttonArea}>
                             <input type="submit" className={styles.buttonSave} value="Salvar alterações" ></input>
                         </div>
