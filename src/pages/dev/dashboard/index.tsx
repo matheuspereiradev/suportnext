5dc7f8b018172bda78adb01cf65cc3e3a4c04fee
Matheus Lima <matheuslima20111997@gmail.com>
2021-08-31 19:49:55 -0300

    layout

--- src/pages/dev/dashboard/index.tsx
+++ src/pages/dev/dashboard/index.tsx
@@ -1,153 +1,71 @@
 import styles from "@styles/dev/dashboard/index.module.scss";
 import { DefaultLayout } from "@layouts/DefaultLayout"
+import { browserAPIRequest, clientAPIRequest } from "@services/api";
+import { parseCookies } from "nookies";
+import { GetServerSideProps } from "next";
+import { format, parseISO } from 'date-fns';
+import { ptBR } from "date-fns/locale";
+import { useState } from "react";
 
-export default function Dashboard() {
+interface User {
+    id: string,
+    name: string,
+    admin: boolean,
+    email: string,
+    gender: string,
+    surname: string
+}
 
-    const sprint = {
-        "id": 2,
-        "name": "SPRINT 001",
-        "startDate": "2021-08-01T03:00:00.000Z",
-        "expectedEndDate": "2021-09-01T03:00:00.000Z",
-        "isOpen": false,
-        "backlogs": [
-            {
-                "id": 2,
-                "title": "Titulao",
-                "description": "descriçãooooo",
-                "isOpen": false,
-                "created_at": "2021-08-28T23:17:32.000Z",
-                "responsable": {
-                    "id": "8ea39402-fffc-4cd9-9174-db7b22515267",
-                    "name": "usuário",
-                    "admin": true,
-                    "email": "usr@mail.co",
-                    "gender": "M",
-                    "surname": "senha 123"
-                },
-                "tasks": [
-                    [
-                        {
-                            "id": 4,
-                            "title": "teste",
-                            "description": "test",
-                            "isBug": true,
-                            "doPosition": 1,
-                            "created_at": "2021-08-30T19:12:01.000Z",
-                            "createdBy": {
-                                "id": "0fbd9c9c-720d-42fe-9eb9-d9a08aff9b22",
-                                "name": "matheus",
-                                "admin": true,
-                                "email": "matheus@mail.com",
-                                "gender": "M",
-                                "surname": "lima"
-                            },
-                            "responsable": {
-                                "id": "0fbd9c9c-720d-42fe-9eb9-d9a08aff9b22",
-                                "name": "matheus",
-                                "admin": true,
-                                "email": "matheus@mail.com",
-                                "gender": "M",
-                                "surname": "lima"
-                            }
-                        }
-                    ],
-                    [
-                        {
-                            "id": 3,
-                            "title": "string",
-                            "description": "string",
-                            "isBug": true,
-                            "doPosition": 2,
-                            "created_at": "2021-08-29T03:18:14.000Z",
-                            "createdBy": {
-                                "id": "0fbd9c9c-720d-42fe-9eb9-d9a08aff9b22",
-                                "name": "matheus",
-                                "admin": true,
-                                "email": "matheus@mail.com",
-                                "gender": "M",
-                                "surname": "lima"
-                            },
-                            "responsable": {
-                                "id": "0fbd9c9c-720d-42fe-9eb9-d9a08aff9b22",
-                                "name": "matheus",
-                                "admin": true,
-                                "email": "matheus@mail.com",
-                                "gender": "M",
-                                "surname": "lima"
-                            }
-                        },
-                        {
-                            "id": 2,
-                            "title": "Titulao",
-                            "description": "descriçãooooo",
-                            "isBug": false,
-                            "doPosition": 2,
-                            "created_at": "2021-08-29T03:04:21.000Z",
-                            "createdBy": {
-                                "id": "0fbd9c9c-720d-42fe-9eb9-d9a08aff9b22",
-                                "name": "matheus",
-                                "admin": true,
-                                "email": "matheus@mail.com",
-                                "gender": "M",
-                                "surname": "lima"
-                            },
-                            "responsable": {
-                                "id": "0fbd9c9c-720d-42fe-9eb9-d9a08aff9b22",
-                                "name": "matheus",
-                                "admin": true,
-                                "email": "matheus@mail.com",
-                                "gender": "M",
-                                "surname": "lima"
-                            }
-                        }
-                    ]
-                ]
-            },
-            {
-                "id": 3,
-                "title": "UC002 - AGUjjjA",
-                "description": "fazer akkk agua",
-                "isOpen": true,
-                "created_at": "2021-08-29T04:04:35.000Z",
-                "responsable": {
-                    "id": "0fbd9c9c-720d-42fe-9eb9-d9a08aff9b22",
-                    "name": "matheus",
-                    "admin": true,
-                    "email": "matheus@mail.com",
-                    "gender": "M",
-                    "surname": "lima"
-                },
-                "tasks": [
-                    [
-                        {
-                            "id": 1,
-                            "title": "do any thing here abaue abaua oi gatinho teste dimensional layout model. The flexbox layout even works when the size of the",
-                            "description": "do it",
-                            "isBug": false,
-                            "doPosition": 1,
-                            "created_at": "2021-08-29T02:35:55.000Z",
-                            "createdBy": {
-                                "id": "8ea39402-fffc-4cd9-9174-db7b22515267",
-                                "name": "usuário",
-                                "admin": true,
-                                "email": "usr@mail.co",
-                                "gender": "M",
-                                "surname": "senha 123"
-                            },
-                            "responsable": {
-                                "id": "0fbd9c9c-720d-42fe-9eb9-d9a08aff9b22",
-                                "name": "matheus",
-                                "admin": true,
-                                "email": "matheus@mail.com",
-                                "gender": "M",
-                                "surname": "lima"
-                            }
-                        }
-                    ]
-                ]
-            }
-        ]
-    };
+interface Tasks {
+    id: number,
+    title: string,
+    description: string,
+    isBug: boolean,
+    doPosition: number,
+    created_at: Date,
+    createdBy: User,
+    responsable: User
+}
+
+interface Backlog {
+    id: number,
+    title: string,
+    description: string,
+    isOpen: boolean,
+    created_at: Date,
+    responsable: User,
+    tasks: Array<Tasks[]>
+}
+
+interface Sprint {
+    id: number,
+    name: string,
+    startDate: Date,
+    expectedEndDate: Date,
+    isOpen: boolean,
+    backlogs: Backlog[]
+}
+interface SprintList {
+    id: number,
+    name: string,
+    startDate: Date,
+    expectedEndDate: Date,
+}
+
+interface DashboardListPros {
+    sprintProp: Sprint;
+    sprintList: SprintList[];
+}
+
+export default function Dashboard({ sprintProp, sprintList }: DashboardListPros) {
+
+    const [sprint, setSprint] = useState<Sprint>(sprintProp);
+
+    async function findSprintDetails(id: number) {
+        console.log('aqui')
+        const details = await browserAPIRequest.get(`/sprint/find/${id}`);
+        setSprint(details.data)
+    }
 
     return (
         <DefaultLayout titleKey="tickets">
@@ -158,10 +76,13 @@ export default function Dashboard() {
                 </div>
                 <div className={styles.content}>
                     <div className={styles.details}>
-                        <label>Sprint</label>
-                        <select>
-                            <option>a</option>
-                            <option>b</option>
+                        <label>Sprint</label><br />
+                        <select className={styles.sprintList} defaultValue={sprint.id} onChange={() => findSprintDetails}>
+                            {
+                                sprintList.map(
+                                    sp => <option key={sp.id} value={sp.id}>{`${sp.id} ${sp.name} - (${sp.startDate}-${sp.expectedEndDate})`}</option>
+                                )
+                            }
                         </select>
                         <button>Adicionar backlog</button>
                     </div>
@@ -169,19 +90,19 @@ export default function Dashboard() {
                         {
                             sprint.backlogs.map(backlog => {
                                 return (
-                                    <div className={styles.backlog}>
+                                    <div className={styles.backlog} key={backlog.id}>
                                         <div className={styles.title}>
                                             <strong>BL{backlog.id}</strong> <span>{backlog.title}</span>
                                             <span className={styles.responsable}>{backlog.responsable.name}</span>
                                         </div>
                                         <div className={styles.body}>
                                             {
-                                                backlog.tasks.map(columns => {
+                                                backlog.tasks.map((columns, index) => {
                                                     return (
-                                                        <div className={styles.columns}>{
+                                                        <div className={styles.columns} key={index}>{
                                                             columns.map(task => {
                                                                 return (
-                                                                    <div className={styles.task}>
+                                                                    <div className={styles.task} key={task.id}>
                                                                         <p>{task.title}</p>
                                                                         <span>{task.responsable.name.split(' ')[0]}</span>
                                                                     </div>
@@ -206,3 +127,41 @@ export default function Dashboard() {
         </DefaultLayout>
     )
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+    const { ['suportewatoken']: token } = parseCookies(ctx);
+
+    if (!token) {
+        return {
+            redirect: {
+                destination: '/login',
+                permanent: false
+            }
+        }
+    }
+
+    const apiClient = clientAPIRequest(ctx)
+
+    const sprints = await apiClient.get('/sprint/open');
+    const sprintInicial = await apiClient.get(`/sprint/find/${sprints.data[0].id}`);
+
+    const sprintListFormatted = sprints.data.map(sp => {
+        return {
+            id: sp.id,
+            name: sp.name,
+            startDate: format(parseISO(sp.startDate), 'dd MMM yyyy', { locale: ptBR }),
+            expectedEndDate: format(parseISO(sp.expectedEndDate), 'dd MMM yyyy', { locale: ptBR }),
+
+        }
+
+    })
+
+    return {
+        props: {
+            sprintProp: sprintInicial.data,
+            sprintList: sprintListFormatted
+        }
+    }
+}
+
